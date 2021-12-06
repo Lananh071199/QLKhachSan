73acf6aa3220b305d316ee577651ed6ce61dafcf
Nguyễn Thành <thanhh16012k@gmail.com>
2021-12-06 15:32:32 +0700

    1223

---
+++ QLKhachSan/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace QLKhachSan.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<QLKhachSan.Models.QLKSDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "QLKhachSan.Models.QLKSDbContext";
+        }
+
+        protected override void Seed(QLKhachSan.Models.QLKSDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
